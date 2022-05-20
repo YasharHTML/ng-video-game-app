ba6a95e35d8d9dc3d49c439e8f7bd9525d182a56
Yashar <yashar220@hotmail.com>
2022-05-20 14:55:07 +0400

    latest commit with weird materialform

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,22 +9,26 @@ import { HttpClientModule } from '@angular/common/http';
 import { GaugeModule } from 'angular-gauge';
 import { MatTabsModule } from '@angular/material/tabs';
 import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
 import { MatIconModule } from '@angular/material/icon';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatSelectModule } from '@angular/material/select';
 import { FormsModule } from '@angular/forms';
 import { SearchBarComponent } from './components/search-bar/search-bar.component';
+import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SearchBarComponent
+    SearchBarComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     HttpClientModule,
+    MatInputModule,
     FormsModule,
     GaugeModule.forRoot(),
     MatTabsModule,
